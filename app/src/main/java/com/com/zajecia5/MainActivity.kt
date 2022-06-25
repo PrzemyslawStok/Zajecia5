00a3ff2dd17b5b08b8da82c4720c7fc245ead71a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-25 09:05:27 +0200

    cd..

--- app/src/main/java/com/com/zajecia5/MainActivity.kt
+++ app/src/main/java/com/com/zajecia5/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.com.zajecia5
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.com.zajecia5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val addDataButton = binding.addDataButton
+
+        addDataButton.setOnClickListener {
+            val intent = Intent(this, InsertData::class.java)
+            startActivity(intent)
+        }
     }
 }
